a3812f1cd0463cc4ceff76664c6750be7c7db875
MJTheSlamDog <gomasterjay@gmail.com>
2025-08-07 15:19:49 +0200

    Dummy data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -10,16 +11,22 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    
-    <section className="main-section">
-      <div className="page-heading">
-        <h1>Track your application & Resume Ratings</h1>
-        <h2>Review your submissions and check AI-powered feedback</h2>
-      </div>
-    </section>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
 
-    
-  </main>;
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your application & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+      </section>
+
+      {resumes.map((resume) => (
+        <div key={resume.id}>
+          <h1>{resume.jobTitle}</h1>
+        </div>
+      ))}
+    </main>
+  );
 }
