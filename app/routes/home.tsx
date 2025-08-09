66fc53cb2a52b694cc0ea974aaa9a3c969776f57
MJTheSlamDog <gomasterjay@gmail.com>
2025-08-09 13:57:11 +0200

    Homepage, Navbar, ResumeCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -16,17 +17,21 @@ export default function Home() {
       <Navbar />
 
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track your application & Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedback</h2>
         </div>
-      </section>
+      
 
-      {resumes.map((resume) => (
-        <div key={resume.id}>
-          <h1>{resume.jobTitle}</h1>
-        </div>
-      ))}
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+      
     </main>
   );
 }
